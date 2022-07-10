7a005942718c809ae7fa24dafe4e1e69d2a2082b
taran <staranbeer@gmail.com>
2022-07-10 23:47:35 +0530

    going to work recreate it

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,5 @@
-import React from "react";
-import Header from "../components/Header";
-import Posts from "../components/Posts/Posts";
-import Stories from "../components/UserStories/Stories";
-import Suggestions from "./../components/Suggestions/Suggestions";
-
 const Home = () => {
-  return (
-    <div className="bg-gray-100 ">
-      <Header />
-      <div className="xs:flex justify-center items-center min-h-screen md:justify-start mx-auto max-w-body sm:px-20 ">
-        <div className="flex flex-col max-w-[470px]">
-          <Stories />
-          <Posts />
-        </div>
-        <div className="suggestions:block hidden flex-1">
-          <Suggestions />
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="bg-gray-100 ">home</div>;
 };
 
 export default Home;
